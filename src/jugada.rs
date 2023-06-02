a47e505f49e95c396b8c04e3f718be60af4dec12
J.P. Filevich <juan.filevich@fing.edu.uy>
2023-06-02 06:09:33 -0300

    +jugada::GritarReTruco

--- src/jugada.rs
+++ src/jugada.rs
@@ -761,12 +761,12 @@ pub struct CantarFlor {
 struct A
   ronda B
 
-B.foo() retornaba un vector xs.
+B.() retornaba un vector xs.
 yo queria obtenerlo de forma mutable
 en una funcion que tenia como arg a &A
 
 fn bar(&A)
-  mut xs = foo()
+  mut xs = ()
 
 no me dejaba hasta que defini fn como `fn bar(&mut A)`
 
@@ -1106,11 +1106,82 @@ impl GritarTruco {
   }
 }
 
-/*
-pub struct Foo {
+pub struct GritarReTruco {
   pub jid: String,
 }
-impl Foo {
+impl GritarReTruco {
+  pub fn id() -> IJugadaId {
+    IJugadaId::JIdTirarCarta
+  }
+  pub fn ok(&self, p:&Partida) -> (Vec<enco::Packet>, bool) {
+    let mut pkts: Vec<enco::Packet> = Vec::new();
+
+    let no_se_fue_al_mazo = !p.ronda.manojo(&self.jid).se_fue_al_mazo;
+    let no_se_esta_jugando_el_envite = p.ronda.envite.estado <= EstadoEnvite::NoCantadoAun;
+    let yo_ouno_de_mis_compas_tiene_flor_yaun_no_canto = p.ronda.hay_equipo_sin_cantar(p.ronda.manojo(&self.jid).jugador.equipo);
+    let la_flor_esta_primero = yo_ouno_de_mis_compas_tiene_flor_yaun_no_canto;
+
+    // CASO I:
+    let truco_gritado = p.ronda.truco.estado == EstadoTruco::Truco;
+    let uno_del_equipo_contrario_grito_truco = truco_gritado && p.ronda.manojo(&p.ronda.truco.cantado_por).jugador.equipo != p.ronda.manojo(&self.jid).jugador.equipo;
+    let caso_i = truco_gritado && uno_del_equipo_contrario_grito_truco;
+
+    // CASO II:
+    let truco_ya_querido = p.ronda.truco.estado == EstadoTruco::TrucoQuerido;
+    let uno_de_mi_equipo_quizo = truco_ya_querido && p.ronda.manojo(&p.ronda.truco.cantado_por).jugador.equipo == p.ronda.manojo(&self.jid).jugador.equipo;
+    // esTurnoDeMiEquipo = p.ronda.get_el_turno().jugador.equipo == p.ronda.manojo(&self.jid).jugador.equipo;
+    let caso_ii = truco_ya_querido && uno_de_mi_equipo_quizo; // && esTurnoDeMiEquipo;
+
+    let re_truco_habilitado = no_se_fue_al_mazo && no_se_esta_jugando_el_envite && (caso_i || caso_ii) && !la_flor_esta_primero;
+
+    if !re_truco_habilitado {
+      if p.verbose {
+        pkts.push(enco::Packet{
+          destination: vec![self.jid.clone()],
+          message: enco::Message(
+            enco::Content::Error {
+              msg: "No es posible cantar re-truco ahora".to_string(),
+            }
+          )
+        });
+      }
+      return (pkts, false);
+    }
+
+    (pkts, true)
+  }
+
+  pub fn hacer(&self, p:&mut Partida) -> Vec<enco::Packet> {
+    let mut pkts: Vec<enco::Packet> = Vec::new();
+    let (mut pre, ok) = self.ok(p);
+    pkts.append(&mut pre);
+
+    if !ok {
+      return pkts
+    }
+
+    if p.verbose {
+      pkts.push(enco::Packet{
+        destination: vec!["ALL".to_string()],
+        message: enco::Message(
+          enco::Content::GritarReTruco {
+            autor: self.jid.clone(),
+          }
+        )
+      });
+    }
+  
+    p.gritar_retruco(&self.jid);
+
+    pkts
+  }
+}
+
+/*
+pub struct  {
+  pub jid: String,
+}
+impl  {
   pub fn id() -> IJugadaId {
     IJugadaId::JIdTirarCarta
   }
